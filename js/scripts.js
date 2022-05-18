6b2dabd7b315079a2d88a5c25fc58e737db4540c
Paul Larin <paullarin24@gmail.com>
2022-05-18 18:09:01 +0800

    add files

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+// burger
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger, .header__menu").toggleClass("show");
+    $("body").toggleClass("lock");
+  });
+});
+
+// header-bg
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
+      $(".header").addClass("white");
+    } else {
+      $(".header").removeClass("white");
+    }
+  });
+});
+
+// accordion
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// carousel slider
+new Splide(".collection__slider", {
+  type: "loop",
+  drag: "free",
+  // focus: "center",
+  autoScroll: {
+    speed: 2,
+  },
+  autoplay: true,
+  rewind: true,
+  pagination: false,
+  arrows: false,
+}).mount();
+
+// tabs
+
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+tabLinks.forEach(function (el) {
+  el.addEventListener("click", openTabs);
+});
+
+function openTabs(el) {
+  var btnTarget = el.currentTarget;
+  var name = btnTarget.dataset.name;
+
+  tabContent.forEach(function (el) {
+    el.classList.remove("show__up");
+  });
+
+  tabLinks.forEach(function (el) {
+    el.classList.remove("show__up");
+  });
+
+  document.querySelector("#" + name).classList.add("show__up");
+
+  btnTarget.classList.add("show__up");
+}
